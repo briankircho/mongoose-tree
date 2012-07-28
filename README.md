c522bdbb5087f4b743293ae5d3198695c93c8454
Brian Kirchoff <briankircho@gmail.com>
2012-07-27 22:34:38 -0700

    Initial commit of mongoose-tree

---
+++ README.md
@@ -0,0 +1,113 @@
+## mongoose-tree
+
+Implements the materialized path strategy for storing a hierarchy of documents with mongoose
+
+# Usage
+
+Install via NPM
+
+    $ npm install mongoose-tree
+
+Then you can use the plugin on your schemas
+
+```javascript
+var tree = require('mongoose-tree');
+
+var UserSchema = new Schema({
+  name : String
+});
+UserSchema.plugin(tree);
+var User = mongoose.model('User', UserSchema);
+
+var adam = new User({ name : 'Adam' });
+var bob = new User({ name : 'Bob' });
+var carol = new User({ name : 'Carol' });
+
+// Set the parent relationships
+bob.parent = adam; 
+carol.parent = bob;
+
+adam.save(function() {
+  bob.save(function() {
+    carol.save();
+  });
+});
+```
+
+At this point in mongoDB you will have documents similar to
+
+    { 
+      "_id" : ObjectId("50136e40c78c4b9403000001"),
+      "name" : "Adam",
+      "path" : "50136e40c78c4b9403000001"
+    }
+    { 
+      "_id" : ObjectId("50136e40c78c4b9403000002"),
+      "name" : "Bob",
+      "parent" : ObjectId("50136e40c78c4b9403000001"),
+      "path" : "50136e40c78c4b9403000001.50136e40c78c4b9403000002"
+    }
+    { 
+      "_id" : ObjectId("50136e40c78c4b9403000003"),
+      "name" : "Carol", 
+      "parent" : ObjectId("50136e40c78c4b9403000002"),
+      "path" : "50136e40c78c4b9403000001.50136e40c78c4b9403000002.50136e40c78c4b9403000003"
+    }
+
+The path is used for recursive methods and is kept up to date by the plugin if the parent is changed
+
+# API
+
+### getChildren
+
+Signature:
+
+    getChildren([recursive], cb);
+
+if recursive is supplied and true subchildren are returned
+
+Based on the above hierarchy:
+
+```javascript
+adam.getChildren(function(err, users) {
+  // users is an array of with the bob document
+});
+
+adam.getChildren(true, function(err, users) {
+  // users is an array with both bob and carol documents
+});
+```
+
+### getAnsestors
+
+Signature:
+
+    getAnsestors(cb);
+
+Based on the above hierarchy:
+
+```javascript
+carol.getAnsestors(function(err, users) {
+  // users is an array of adam and bob
+})
+```
+
+### level
+
+Equal to the level of the hierarchy
+
+```javascript
+carol.level; // equals 3
+```
+
+# Tests
+
+To run the tests install mocha
+
+    npm install mocha -g
+
+and then run
+
+    mocha
+
+
---
+++ test/tree.js
@@ -0,0 +1,193 @@
+var assert = require('assert'),
+    mongoose = require('mongoose'),
+    tree = require('../lib/tree'),
+    async = require('async'),
+    should = require('should'),
+    _ = require('underscore');
+
+var Schema = mongoose.Schema;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mongoose-tree');
+
+describe('tree tests', function() {
+  // Schema for tests
+  var UserSchema = new Schema({
+    name : String
+  });
+  UserSchema.plugin(tree);
+  var User = mongoose.model('User', UserSchema);
+
+  // Set up the fixture
+  beforeEach(function(done) {
+    User.remove({}, function(err) {
+      should.not.exist(err);
+
+      var adam = new User({ 'name' : 'Adam' });
+      var bob = new User({ 'name' : 'Bob', 'parent' : adam });
+      var carol = new User({ 'name' : 'Carol', 'parent' : adam });
+      var dann = new User({ 'name' : 'Dann', 'parent' : carol });
+      var emily = new User({ 'name' : 'Emily', 'parent' : dann });
+
+      async.forEachSeries([adam, bob, carol, dann, emily], function(doc, cb) {
+        doc.save(cb);
+      }, done);
+    });
+  });
+
+  describe('adding documents', function() {
+    it('should set parent id and path', function(done) {
+      User.find({}, function(err, users) {
+        should.not.exist(err);
+
+        var names = {};
+        users.forEach(function(user) {
+          names[user.name] = user;
+        });
+        
+        should.not.exist(names['Adam'].parent);
+        names['Bob'].parent.toString().should.equal(names['Adam']._id.toString());
+        names['Carol'].parent.toString().should.equal(names['Adam']._id.toString());
+        names['Dann'].parent.toString().should.equal(names['Carol']._id.toString());
+        names['Emily'].parent.toString().should.equal(names['Dann']._id.toString());
+
+        var expectedPath = [names['Adam']._id, names['Carol']._id, names['Dann']._id].join('.');
+        names['Dann'].path.should.equal(expectedPath);
+
+        done();
+      });
+    });
+  });
+
+  describe('removing document', function() {
+    it('should remove leaf nodes', function(done) {
+      User.findOne({ name : 'Emily' }, function(err, emily) {
+        emily.remove(function(err) {
+          should.not.exist(err);
+
+          User.find({}, ['name'], function(err, users) {
+            should.not.exist(err);
+            users.length.should.equal(4);
+            _.pluck(users, 'name').should.not.include('Emily');
+            done();
+          });
+        });
+      });
+    });
+    it('should remove all children', function(done) {
+      User.findOne({ name : 'Carol' }, function(err, user) {
+        should.not.exist(err);
+
+        user.remove(function(err) {
+          should.not.exist(err);
+
+          User.find({}, ['name'], function(err, users) {
+            should.not.exist(err);
+
+            users.length.should.equal(2);
+            _.pluck(users, 'name').should.include('Adam').and.include('Bob');
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  function checkPaths(done) {
+    User.find({}, function(err, users) {
+      should.not.exist(err);
+
+      var ids = {};
+      users.forEach(function(user) {
+        ids[user._id] = user;
+      });
+
+      users.forEach(function(user) {
+        if(!user.parent) {
+          return;
+        }
+        should.exist(ids[user.parent]);
+        user.path.should.equal(ids[user.parent].path+"."+user._id);
+      });
+
+      done();
+    });
+  }
+
+  describe('moving documents', function() {
+    it('should change children paths', function(done) {
+      User.find({}, function(err, users) {
+        should.not.exist(err);
+
+        var names = {};
+        users.forEach(function(user) {
+          names[user.name] = user;
+        });
+        
+        var carol = names['Carol'];
+        var bob = names['Bob'];
+
+        carol.parent = bob;
+        carol.save(function(err) {
+          should.not.exist(err);
+
+          checkPaths(done);
+        });
+      });
+    });
+  });
+
+  describe('get children', function() {
+    it('should return immediate children', function(done) {
+      User.findOne({ 'name' : 'Adam' }, function(err, adam) {
+        should.not.exist(err);
+
+        adam.getChildren(function(err, users) {
+          should.not.exist(err);
+
+          users.length.should.equal(2);
+          _.pluck(users, 'name').should.include('Bob').and.include('Carol');
+          done();
+        });
+      });
+    });
+    it('should return recursive children', function(done) {
+      User.findOne({ 'name' : 'Carol' }, function(err, carol) {
+        should.not.exist(err);
+
+        carol.getChildren(true, function(err, users) {
+          should.not.exist(err);
+
+          users.length.should.equal(2);
+          _.pluck(users, 'name').should.include('Dann').and.include('Emily');
+          done();
+        });
+      });
+    });
+  });
+
+  describe('level virtual', function() {
+    it('should equal the number of ancestors', function(done) {
+      User.findOne({ 'name' : 'Dann' }, function(err, dann) {
+        should.not.exist(err);
+
+        dann.level.should.equal(3);
+        done();
+      });
+    });
+  });
+
+  describe('get ansestors', function() {
+    it('should return ancestors', function(done) {
+      User.findOne({ 'name' : 'Dann' }, function(err, dann) {
+        dann.getAnsestors(function(err, ansestors) {
+          should.not.exist(err);
+
+          ansestors.length.should.equal(2);
+          _.pluck(ansestors, 'name').should.include('Carol').and.include('Adam');
+          done();
+        });
+      });
+    });
+  });
+
+});
